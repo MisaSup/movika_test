0e34a54e4e46d7ecae58d281aeada1ebd1053ee2
misha_wcarlos <miguelito2135@gmail.com>
2022-04-24 13:12:14 +0300

    workin w rought

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import rough from 'roughjs/bundled/rough.esm';
+
+const  generator = rough.generator();
+
+function createElement(x1, y1, x2, y2)
+{
+  const rcElement = generator.line(x1, y1, x2, y2);
+  return {x1, y1, x2, y2, rcElement};
+}
 
 function App() {
+  const [drawing, setDrawing] = useState(false);
+  const [elements, setElem] = useState([]);
+
+  React.useEffect(() => {
+    const canva = document.getElementById('cc'),
+    ctx = canva.getContext('2d');
+    console.log(canva);
+    ctx.clearRect(10, 10, canva.width, canva.height);
+
+
+    let roughCanvas = rough.canvas(canva);
+
+    elements.forEach( ({ rcElement }) => {
+      roughCanvas.draw(rcElement)
+    }, elements);
+  });
+
+
+  const handleMouseDown = (e) => 
+  {
+    setDrawing(true);
+    const {clientX, clientY} = e;
+    const elem = createElement(clientX, clientY, clientX, clientY);
+    setElem(prevState => [...prevState, elem]);
+  }
+  
+  const handleMouseMove = (e) =>
+  {
+      if(!drawing) return;
+
+      const {clientX, clientY} = e;
+      let index = elements.length - 1;
+      const {x1, y1} = elements[index];
+      const updatedElem = createElement(x1, y1, clientX, clientY);
+
+      const elemCopy = [...elements];
+      elemCopy[index] = updatedElem;
+      setElem(elemCopy);
+
+  }
+
+  const handleMouseUp = (e) =>
+  {
+    setDrawing(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id = 'cc'
+      height={window.innerHeight}
+      width={window.innerWidth}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}>
+
+      </canvas>
     </div>
   );
 }
