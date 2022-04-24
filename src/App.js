05dfd33ee03d4b5089e9d239fb24736ecb339955
misha_wcarlos <miguelito2135@gmail.com>
2022-04-24 22:01:15 +0300

    ver 0.2

--- src/App.js
+++ src/App.js
@@ -1,71 +1,115 @@
 import './App.css';
-import React, { useState } from 'react';
-import rough from 'roughjs/bundled/rough.esm';
+import React from 'react';
+import {ReactComponent as MySvg} from './1_1.svg';
 
-const  generator = rough.generator();
-
-function createElement(x1, y1, x2, y2)
+function getPoints(str)
 {
-  const rcElement = generator.line(x1, y1, x2, y2);
-  return {x1, y1, x2, y2, rcElement};
+  let coordinates = str,
+  x, y;
+
+  if (/m/i.test(str))
+  {
+    coordinates = coordinates.split('').slice(1).join('').split(',');
+  }
+  else 
+  {
+    coordinates = coordinates.split(',');
+  }
+  x = +coordinates[0];
+  y = +coordinates[1];
+  return {x, y};
+}
+
+function checkPoints(x, y, points)
+{
+  let result = 0;
+
+  if (x >= points.x - 15 && x <= points.x + 15)
+  {
+    result++;
+  }
+  if (y >= points.y - 15 && y <= points.y + 15)
+  {
+    result++;
+  }
+  return (result === 2 ? true : false)  
 }
 
 function App() {
-  const [drawing, setDrawing] = useState(false);
-  const [elements, setElem] = useState([]);
-
   React.useEffect(() => {
     const canva = document.getElementById('cc'),
-    ctx = canva.getContext('2d');
-    console.log(canva);
-    ctx.clearRect(10, 10, canva.width, canva.height);
-
-
-    let roughCanvas = rough.canvas(canva);
-
-    elements.forEach( ({ rcElement }) => {
-      roughCanvas.draw(rcElement)
-    }, elements);
-  });
-
-
-  const handleMouseDown = (e) => 
-  {
-    setDrawing(true);
-    const {clientX, clientY} = e;
-    const elem = createElement(clientX, clientY, clientX, clientY);
-    setElem(prevState => [...prevState, elem]);
-  }
-  
-  const handleMouseMove = (e) =>
-  {
-      if(!drawing) return;
-
+          ctx = canva.getContext('2d'),
+          path = document.querySelector('[d]').attributes[0].textContent;
+    
+    let curvePath = new Path2D(path),
+        hitCounter = 0,
+        drawing = false,
+        splitPath = path.split(' '),
+        startPoints = getPoints(splitPath[0]), 
+        endPoints = getPoints(splitPath[splitPath.length - 1]),
+        startCheck = false, 
+        endCheck = false;
+    
+    ctx.strokeStyle = 'white';
+    ctx.stroke(curvePath);
+    console.log(startPoints, endPoints);
+    const handleMouseDown = (e) => 
+    {
+      drawing = true;
+      ctx.beginPath();
       const {clientX, clientY} = e;
-      let index = elements.length - 1;
-      const {x1, y1} = elements[index];
-      const updatedElem = createElement(x1, y1, clientX, clientY);
+      ctx.moveTo(
+        clientX - canva.offsetLeft,
+        clientY - canva.offsetTop);
+    }
+    
+    const handleMouseMove = (e) =>
+    {
+      if(drawing)
+      {
+        const {clientX, clientY} = e;
+        ctx.lineTo(
+          clientX - canva.offsetLeft,
+          clientY - canva.offsetTop);
+        if (ctx.isPointInPath(curvePath, clientX, clientY))
+        {
+          hitCounter++;
+          if (checkPoints(clientX, clientY, startPoints)) startCheck = true;
+          if (checkPoints(clientX, clientY, endPoints)) endCheck = true;
+        }
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 3;
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        ctx.stroke();
+      }
+    }
+    
+    const handleMouseUp = (e) =>
+    {
+      drawing = false;
+      if (startCheck && endCheck && hitCounter >= 100)
+      {
+        console.log("YOU GOT IT");
+      }
+      console.log(startCheck, endCheck, hitCounter);
+      hitCounter = 0;
+      startCheck = false;
+      endCheck = false;
+    }
 
-      const elemCopy = [...elements];
-      elemCopy[index] = updatedElem;
-      setElem(elemCopy);
+    canva.addEventListener('pointerdown', handleMouseDown);
+    canva.addEventListener('pointermove', handleMouseMove);
+    canva.addEventListener('pointerup', handleMouseUp);
+  })
 
-  }
-
-  const handleMouseUp = (e) =>
-  {
-    setDrawing(false);
-  }
 
   return (
     <div className="App">
+      <MySvg display='none' />
       <canvas id = 'cc'
       height={window.innerHeight}
-      width={window.innerWidth}
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}>
-
+      width={window.innerWidth}>
       </canvas>
     </div>
   );
